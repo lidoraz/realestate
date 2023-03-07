3d2ae66d90bf483c9395ae29e46e4ecd4e3fbd64
lidorazulay <lidor@upstream.auto>
2023-03-07 22:57:34 +0200

    small changes

--- scrape_yad2/config.py
+++ scrape_yad2/config.py
@@ -21,10 +21,8 @@ cols_renamer_today = {
     "neighborhood": "neighborhood",
     "street": "street",
     "title_1": "street_num",
-    # "primaryarea": "primary_area",
     "primaryareaid": "primary_area_id",
     "area_id": "area_id",
-    # "city_code": "city_id",
     "merchant": "is_agency",
     "merchant_name": "agency_name",
     "img_url": "img_url",
@@ -34,40 +32,6 @@ cols_renamer_today = {
     "date_added": "date_added",
     "processing_date": "processing_date"  # Added
 }
-
-
-forsale_today_cols = ['line_1', 'line_2', 'line_3', 'row_1', 'row_2', 'row_3', 'row_4',
-                      'search_text', 'title_1', 'title_2', 'images_count', 'img_url',
-                      'images_urls', 'video_url', 'primaryarea', 'primaryareaid',
-                      'areaid_text', 'secondaryarea', 'area_id', 'city', 'city_code',
-                      'street', 'coordinates', 'geohash', 'ad_highlight_type',
-                      'background_color', 'highlight_text', 'order_type_id', 'ad_number',
-                      'cat_id', 'customer_id', 'feed_source', 'id', 'link_token', 'merchant',
-                      'contact_name', 'merchant_name', 'record_id', 'subcat_id', 'currency',
-                      'price', 'date', 'date_added', 'date_updated', 'promotional_ad',
-                      'address_more', 'hood_id',  # 'office_about', 'office_logo_url',
-                      'square_meters', 'hometypeid_text', 'neighborhood',
-                      'assetclassificationid_text', 'rooms_text', 'aboveprice', 'is_platinum',
-                      'is_mobile_platinum', 'processing_date']
-# Can use this to classify::
-# 'PrimaryArea': 'hamerkaz_area'
-# 'AreaID_text': 'אזור רמת גן וגבעתיים'
-
-to_be_removed_cols = ['merchant_name', 'currency', 'office_about', 'office_logo_url', ]
-rent_today_cols = ['line_1', 'line_2', 'line_3', 'row_1', 'row_2', 'row_3', 'row_4',
-                   'search_text', 'title_1', 'title_2', 'images_count', 'img_url',
-                   'images_urls', 'video_url', 'primaryarea', 'primaryareaid',
-                   'areaid_text', 'secondaryarea', 'area_id', 'city', 'city_code',
-                   'street', 'coordinates', 'geohash', 'ad_highlight_type',
-                   'background_color', 'highlight_text',
-                   'order_type_id', 'ad_number',
-                   'cat_id', 'customer_id', 'feed_source', 'id', 'link_token', 'merchant',
-                   'contact_name', 'merchant_name',
-                   'record_id', 'subcat_id', 'currency',
-                   'price', 'date', 'date_added', 'date_updated',
-                   'address_more', 'hood_id', 'office_about', 'office_logo_url',
-                   'square_meters', 'hometypeid_text', 'neighborhood',
-                   'assetclassificationid_text', 'rooms_text', 'aboveprice', 'processing_date']
 
 sql_today_dtypes = {'id': String, 'asset_type': String,
                     'price': BigInteger, 'city': String, 'rooms': Float,
@@ -97,3 +61,37 @@ sql_items_dtypes = {'id': String, 'parking': Integer, 'balconies': Boolean, 'num
 
 q_history_last_price = """SELECT id, price as last_price from (select id, price, processing_date, ROW_NUMBER() over (partition by id order by processing_date desc)
  as rn from {}) a where rn=1"""
+
+
+# forsale_today_cols = ['line_1', 'line_2', 'line_3', 'row_1', 'row_2', 'row_3', 'row_4',
+#                       'search_text', 'title_1', 'title_2', 'images_count', 'img_url',
+#                       'images_urls', 'video_url', 'primaryarea', 'primaryareaid',
+#                       'areaid_text', 'secondaryarea', 'area_id', 'city', 'city_code',
+#                       'street', 'coordinates', 'geohash', 'ad_highlight_type',
+#                       'background_color', 'highlight_text', 'order_type_id', 'ad_number',
+#                       'cat_id', 'customer_id', 'feed_source', 'id', 'link_token', 'merchant',
+#                       'contact_name', 'merchant_name', 'record_id', 'subcat_id', 'currency',
+#                       'price', 'date', 'date_added', 'date_updated', 'promotional_ad',
+#                       'address_more', 'hood_id',  # 'office_about', 'office_logo_url',
+#                       'square_meters', 'hometypeid_text', 'neighborhood',
+#                       'assetclassificationid_text', 'rooms_text', 'aboveprice', 'is_platinum',
+#                       'is_mobile_platinum', 'processing_date']
+# # Can use this to classify::
+# # 'PrimaryArea': 'hamerkaz_area'
+# # 'AreaID_text': 'אזור רמת גן וגבעתיים'
+#
+# to_be_removed_cols = ['merchant_name', 'currency', 'office_about', 'office_logo_url', ]
+# rent_today_cols = ['line_1', 'line_2', 'line_3', 'row_1', 'row_2', 'row_3', 'row_4',
+#                    'search_text', 'title_1', 'title_2', 'images_count', 'img_url',
+#                    'images_urls', 'video_url', 'primaryarea', 'primaryareaid',
+#                    'areaid_text', 'secondaryarea', 'area_id', 'city', 'city_code',
+#                    'street', 'coordinates', 'geohash', 'ad_highlight_type',
+#                    'background_color', 'highlight_text',
+#                    'order_type_id', 'ad_number',
+#                    'cat_id', 'customer_id', 'feed_source', 'id', 'link_token', 'merchant',
+#                    'contact_name', 'merchant_name',
+#                    'record_id', 'subcat_id', 'currency',
+#                    'price', 'date', 'date_added', 'date_updated',
+#                    'address_more', 'hood_id', 'office_about', 'office_logo_url',
+#                    'square_meters', 'hometypeid_text', 'neighborhood',
+#                    'assetclassificationid_text', 'rooms_text', 'aboveprice', 'processing_date']
