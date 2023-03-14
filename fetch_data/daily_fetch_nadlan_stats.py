d9d2dba0ca29ab4f7f2d3d0fcafc01ab7e095ef6
lidorazulay <lidor@upstream.auto>
2023-03-15 00:35:15 +0200

    stats add

---
+++ fetch_data/daily_fetch_nadlan_stats.py
@@ -0,0 +1,169 @@
+import matplotlib.pyplot as plt
+import pandas as pd
+
+plt.style.use('ggplot')
+timeline_size = (14, 5)
+from scrape_nadlan.utils_insert import get_engine
+
+used_cols = ["trans_date", "city", "n_rooms", "price_declared",
+             "sq_m_gross", "sq_m_net", "floor", "n_floors",
+             "year_built", "parking", "lat", "long",
+             'gush', 'helka'
+             ]
+q = """SELECT {cols_str} from nadlan_trans where 
+trans_date > TO_DATE('{from_date}', 'YYYY-MM-DD')
+and price_declared = price_estimated
+and deal_part = 1
+"""
+
+
+def get_data_nadlan(eng):
+    q_frmt = q.format(cols_str=', '.join(used_cols), from_date='2015-01-01')
+    with eng.connect() as conn:
+        df = pd.read_sql(q_frmt, conn)
+    df['trans_date'] = pd.to_datetime(df['trans_date'])
+    df = df.set_index('trans_date')
+    return df
+
+
+def print_recent(df):
+    from datetime import datetime
+    print(df.index.min(), df.index.max(), (datetime.now() - df.index.max()).days)
+    print(df.groupby(df.index).size()[-5:].sort_index(ascending=False).to_frame())
+
+
+def get_ir():
+    df_rates = pd.read_csv("../resources/il_ir.csv")
+    df_rates['dt'] = pd.to_datetime(df_rates['dt'])
+    df_rates = df_rates.set_index('dt')
+    df_rates = df_rates.sort_index()
+    return df_rates
+
+
+def plot_timeline_new_vs_old(df, resample_rule, df_rates=None):
+    df['year_sold_built_diff'] = df.index.year - df['year_built']
+    df['newOrUsed'] = df['year_sold_built_diff'].apply(lambda x: "new" if x <= 0 else "used")
+    res = df.resample(resample_rule, origin='end')['newOrUsed'].value_counts().unstack()[:-1]
+    fig, ax = plt.subplots(figsize=timeline_size)
+    # make a plot
+    ax.plot(res.index, res['new'], marker="*", label='new')
+    ax.plot(res.index, res['used'], marker="*", label='used')
+    ax.set_xlabel("year", fontsize=14)
+    ax.set_ylabel("# Units", fontsize=14)
+    ax.legend()
+    # twin object for two different y-axis on the sample plot
+    if df_rates is not None:
+        ax2 = ax.twinx()
+        # make a plot with different y-axis using second axis object
+        ax2.plot(df_rates.index, df_rates['p'], color="gray", label='prime')
+        ax2.set_ylabel("prime", fontsize=14)
+        ax2.legend()
+    fig.savefig(f'plots_daily_nadlan/timeline_new_vs_old.png',
+                dpi=100,
+                bbox_inches='tight')
+    plt.close()
+
+
+def plot_timeline_rooms(df):
+    ax = df.resample('M')['n_rooms_'].value_counts().unstack().plot(kind='area', stacked=True, figsize=timeline_size)
+    fig = ax.get_figure()
+    fig.savefig('plots_daily_nadlan/timeline_rooms.png', dpi=100,
+                bbox_inches='tight')
+    plt.close()
+
+
+def plot_timeline_rooms_median_prices(df):
+    df.groupby('n_rooms_').resample('M', convention='end', closed="left")['price_declared'].median().unstack(0)[
+        ['3', '4', '5']].plot(figsize=timeline_size)
+    # df.resample('M', convention='end', closed="left")['price_declared'].median().plot()
+    plt.ylim([0, None])
+    plt.savefig('plots_daily_nadlan/timeline_rooms_median_price.png', dpi=100,
+                bbox_inches='tight')
+    plt.close()
+
+
+def plot_timeline_rooms_city(df):
+    dff = df.groupby(['city_', 'n_rooms_']).resample('M', convention='end', closed="left")['price_declared'].agg(
+        ['mean', 'size', 'std', 'median'])  # .mean() # .T.pct_change().dropna()
+    # colors = ['Red', 'Blue', 'Orange', 'Yellow', 'Green', 'Gray', 'Black', 'Lime']
+    metric = "median"
+    title_suff = ' מחיר חציוני '[::-1]
+    for idx, tup in enumerate(dff.groupby(level=0)):
+        row_id, data = tup
+        city = row_id
+        pivot = data.reset_index().pivot(index='trans_date', columns='n_rooms_', values=metric)
+        pivot.drop(columns=['+6', '-2']).plot(kind='line', figsize=timeline_size, title=title_suff + city[::-1])
+        plt.savefig(f'plots_daily_nadlan/timeline_rooms_city_{city}.png')
+        plt.close()
+
+
+def _plot_colorize_pct(df, reverse=True, axis=None):
+    # https://stackoverflow.com/questions/38246559/how-to-create-a-heat-map-in-python-that-ranges-from-green-to-red
+    from matplotlib.colors import LinearSegmentedColormap
+    c = ["darkred", "red", "lightcoral", "white", "palegreen", "green", "darkgreen"]
+    if reverse:
+        c = c[::-1]
+    v = [0, .1, .4, .5, .6, .9, 1.]
+    l = list(zip(v, c))
+    cmap = LinearSegmentedColormap.from_list('rg', l, N=256)
+    return df.style.background_gradient(cmap, axis=axis, vmin=-0.5, vmax=0.5).format('{:,.2%}')
+
+
+def get_n_sales(df):
+    # cmap=LinearSegmentedColormap.from_list('rg',["darkred", "w", "darkgreen"], N=256)
+    print("Number of sales")
+    # [cond_yeshuv_most & cond_full_sale]
+    dff = df.groupby(['city_']).resample('Q', convention='end', closed="left").size().T.pct_change().dropna()
+    # display(dff)
+    _plot_colorize_pct(dff).to_html("plots_daily_nadlan/n_sales.html")
+
+
+def get_monthly_counts(df):
+    # Was in Keshet 12, can tell a change in pct of sells between month to month, compared to rest
+    df_monthly_counts = df.groupby(['city_']).resample('Q', convention='end', closed="left").size().T
+    df_monthly_counts = df_monthly_counts.divide(df_monthly_counts.sum(axis=1).values, axis=0)
+    df_style = df_monthly_counts.style.background_gradient('RdBu', axis=0).format('{:,.2%}')
+    df_style.to_html("plots_daily_nadlan/monthly_counts.html")
+
+
+def get_pct_change(df):
+    print("Change in price")
+    dff = df.groupby(['city_']).resample('Q', convention='end', closed="left")[
+        'price_declared'].mean()  # .T.pct_change().dropna()
+    _plot_colorize_pct(dff.unstack().T.pct_change().dropna()).to_html("plots_daily_nadlan/pct_change.html")
+
+
+def add_columns(df):
+    def apply_room(x):
+        if x >= 6:
+            return '+6'
+        elif x <= 2.5:
+            return '-2'
+        else:
+            return str(int(x))
+
+    df['n_rooms_'] = df['n_rooms'].apply(apply_room)
+    limit_cities = 9
+    yeshuv_lst = df['city'].value_counts()[:limit_cities].index.tolist()
+    df['city_'] = df['city'].apply(lambda x: x if x in yeshuv_lst else "_השאר")
+    return df
+
+
+def run():
+    # eng = get_engine()
+    # df = get_data_nadlan(eng)
+    # df.to_pickle("nadlan.pk")
+    df = pd.read_pickle("nadlan.pk")
+    print_recent(df)
+    plot_timeline_new_vs_old(df, '14D', get_ir())
+    df = add_columns(df)
+    get_monthly_counts(df)
+    get_n_sales(df)
+    get_pct_change(df)
+    plot_timeline_rooms(df)
+    plot_timeline_rooms_median_prices(df)
+    plot_timeline_rooms_city(df)
+
+
+if __name__ == '__main__':
+    run()
---
+++ fetch_data/daily_fetch_stats.py
@@ -0,0 +1,184 @@
+from scrape_nadlan.utils_insert import get_engine
+import pandas as pd
+from datetime import datetime, timedelta
+import matplotlib.pyplot as plt
+import plotly.graph_objects as go
+
+plt.style.use('ggplot')
+
+log_tbl_cols = ['id', 'processing_date', 'date_updated', 'date_added', 'price', 'asset_type', 'rooms',
+                'asset_status', 'active', 'city',
+                # 'neighborhood', 'street', 'square_meters', 'is_agency',  'lat', 'long'
+                ]
+
+
+def get_heb_type(type_):
+    return "שהושכרה" if type_ == "rent" else "שנמכרה"
+
+
+def fetch_data_log(type_, eng):
+    with eng.connect() as conn:
+        df = pd.read_sql(f"SELECT {','.join(log_tbl_cols)} FROM public.yad2_{type_}_log", conn)  #
+        df['processing_date'] = pd.to_datetime(df['processing_date'])
+        df = df.set_index('processing_date')
+        print(f"Log Table stats: {df.index.min()}, {df.index.max()}")
+        return df
+
+
+# Can create also historic ratio as active will be all relevant items and those who closed AFTER the date.
+def create_ratio(df, days_back=21, min_samples=200):
+    rent_active = df[df['active']].groupby('city').size()
+    rent_active = rent_active[rent_active >= min_samples].rename('active')
+    rent_unactive = df[
+        (~df['active']) & (df.index >= datetime.today() - timedelta(days=days_back))].groupby(
+        'city').size()
+    rent_unactive = rent_unactive.rename('unactive')
+    df_r = rent_unactive.to_frame().join(rent_active)
+    df_r['r'] = df_r.apply(lambda r: (r['active']) / r['unactive'], axis=1).rename('active_to_taken')
+    df_r = df_r[~pd.isna(df_r['active'])]
+    df_r = df_r.sort_values('r', ascending=False)
+    res = df_r['r']
+    return res
+
+
+def run_for_cities(df, type_, n_cities=9, resample_rule='7D'):
+    sel_cities = df['city'].value_counts().index[:n_cities].to_list()
+    df['city_'] = df['city'].apply(lambda x: x if x in sel_cities else "rest")
+    for city, grp in df.groupby('city_'):
+        create_percentiles_per_city(df, city, type_, resample_rule)
+
+
+def create_percentiles_per_city(df, city, type_, resample_rule):
+    # df_f = df_t.query(f"city == '{c}' and active == False and  rooms_text == 3.0")
+    df = df.query(f"city_ == '{city}' and active == False")
+    x = df.resample(resample_rule, origin='end')['price'].describe()  # agg(['median', 'mean', 'std', 'size'])
+    heb_type = "שכירות" if type_ == 'rent' else "מכירה"
+    title = f"{city} {heb_type} אחוזון {resample_rule[::-1]}"[::-1]
+    fig, ax = plt.subplots(figsize=(20, 10))
+    x[['25%', '50%', '75%']].plot(figsize=(8, 4), kind='line', stacked=False, title=title, ax=ax, marker='o')
+    # plt.ylim([0, None])
+    x.plot(y='count', ax=ax, secondary_y=True, linestyle='-.')
+    plt.savefig(f'plots_daily_{type_}/percentile_{city}.png')
+    # plt.show()
+    plt.close()
+
+
+def get_price_changes(eng, type_):
+    q = f"""
+    with t0 as (
+    SELECT id, count(*) as cnt from yad2_{type_}_history where price > 0 group by id order by cnt desc),
+    t1 as (SELECT b.* from t0 a join yad2_{type_}_history b on a.id = b.id  where cnt > 1)
+    select * from t1 where id in (SELECT id from yad2_{type_}_today)
+    """
+    with eng.connect() as conn:
+        df_rent_prices = pd.read_sql(q, conn)
+    df_rent_prices = df_rent_prices.sort_values(['id', 'processing_date'])
+    df_rent_g = df_rent_prices.groupby('id').agg(price_lst=('price', list),
+                                                 dt_lst=('processing_date', list),
+                                                 dt_last=('processing_date', 'last'))
+    df_rent_g['price_pct'] = df_rent_g['price_lst'].apply(lambda x: x[-1] / x[0] - 1)
+    df_rent_g['days'] = (df_rent_g['dt_lst'].apply(lambda x: x[-1] - x[0])).dt.days
+    df_rent_g['dt_last'] = pd.to_datetime(df_rent_g['dt_last'])
+    df_g = df_rent_g[df_rent_g['dt_last'] > datetime.now() - pd.to_timedelta('8D')].query("price_pct > -0.8")
+    res = df_g.reset_index().sort_values(['price_pct', 'days'], ascending=[True, False])[:30]
+    res.to_html(f'plots_daily_{type_}/price_changes.html')
+
+
+def plot_ratio(res, type_):
+    res_ = res.copy()
+    res_.index = [x[::-1] for x in res.index]
+    heb_type = "שהושכרה" if type_ == "rent" else "שנמכרה"
+    res_.plot(kind='bar', figsize=(12, 5),
+              title=f"{datetime.today().date()} " + f"יחס הדירות הפנויות לכל דירה {heb_type} בחודש האחרון "[::-1])
+    print("lower is better for rent as more demand")
+    _ = plt.xticks(rotation=60)
+    plt.tight_layout()
+    plt.savefig(f'plots_daily_{type_}/active_ratio.png')
+    plt.close()
+
+
+def plot_ratio_f(res, type_):
+    res_ = res.copy()
+    heb_type = "שהושכרה" if type_ == "rent" else "שנמכרה"
+    title = f"{datetime.today().date()} " + f"יחס הדירות הפנויות לכל דירה {heb_type} בחודש האחרון "
+    fig = go.Figure(data=go.Bar(x=res_.index, y=res_))  # hover text goes here
+    fig.update_layout(title=title).update_xaxes(tickangle=300)
+    fig.show()
+
+
+def plot_scatter_f(df, res_ratio, type_):
+    df['time_alive'] = (datetime.today() - df['date_added']).dt.days
+    df_g = df[~df['active']].groupby('city')['time_alive'].agg(['mean', 'std', 'median', 'size'])
+    df_g = df_g.join(res_ratio, how='inner')
+    len_b = len(df_g)
+    df_g = df_g[df_g['size'] >= 30]
+    print(len_b, len(df_g))
+    fig = go.Figure(data=go.Scatter(x=df_g['median'], y=df_g['r'], marker_color=df_g['size'],
+                                    mode='markers+text',
+                                    textposition="bottom center",
+                                    hovertemplate="%{text}<br>#Days: %{x}</br>#Deals: %{marker.color}",
+                                    name="",
+                                    text=df_g.index))  # hover text goes here
+    fig.update_layout(title=f"פיזור זמן מול היחס על דירה {get_heb_type(type_)}",
+                      xaxis_title="זמן חציוני לדירות באתר מרגע הפרסום עד להורדה",
+                      yaxis_title="יחס דירות באתר מול דירות שירדו בחודש האחרון",
+                      dragmode='pan')
+    fig.show()
+    #
+    # fig, ax = plt.subplots(figsize=(12, 12))
+    # ax.scatter(df_g['median'], df_g['r'])
+    # ax.set_title("פיזור זמן מול היחס"[::-1])
+    # ax.set_ylabel('יחס דירות באתר מול דירות שירדו בחודש האחרון'[::-1])
+    # ax.set_xlabel('זמן חציוני לדירות באתר מרגע הפרסום עד להורדה'[::-1])
+    #
+    # for i, txt in enumerate(df_g.index):
+    #     ax.annotate(txt[::-1], (df_g['median'][i] * 1.01, df_g['r'][i]))
+    # plt.savefig(f'plots_daily_{type_}/scatter.png')
+    # plt.close()
+
+
+def test_1(df):
+    grp_by = ['city', 'rooms', 'asset_status']
+    filter_by = 'active == False and rooms == 3.0'
+    agg_what = 'price'
+    agg_by = ['mean', 'std', 'size']
+    having_by = 'size > 15'
+    df.query(filter_by).groupby(grp_by)[agg_what].agg(agg_by).round().query(having_by)  # .mean()
+
+
+def get_compare_closed_vs_active_median_price(df, city):
+    # city = 'גבעתיים'
+    # city = 'הרצליה'
+    # city = 'רמת גן'
+    # city = 'באר שבע'
+    # city = 'קרית ים'
+    # FILTER HERE BY ACTIVE LAST MONTH.
+    # and price > 1000
+    # q = f"city == '{city}' and active == True| and rooms_text <= 5 "
+    print(city)
+    q = f"city == '{city}' and 3.0 <= rooms <= 4 "
+    df_g_p = df.query(q).groupby(['rooms', 'asset_status', 'active'])['price'].agg(
+        ['median', 'mean', 'std', 'size']).query(
+        'size > 5').round()  # .agg(['mean', 'median', len]) # .hist(stacked=True)
+    df_g_p.name = city
+    print(df_g_p)
+
+
+def run(type_):
+    print(f"Started daily stats for {type_}")
+    # eng = get_engine()
+    # df = fetch_data_log(type_, eng)
+    fname = f'test_log_{type_}.pk'
+    df = pd.read_pickle(fname)
+    df.to_pickle(fname)
+    res_ratio = create_ratio(df, days_back=30, min_samples=200)
+    # plot_ratio_f(res_ratio, type_)
+    plot_scatter_f(df, res_ratio, type_)
+    exit()
+    run_for_cities(df, type_, n_cities=9, resample_rule='7D')
+    get_price_changes(eng, type_)
+
+
+if __name__ == '__main__':
+    # run('forsale')
+    run('rent')
