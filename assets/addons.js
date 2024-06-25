670b0b25ce6f3f18a78cadb6625cf2b81720c642
lidor <lidor.az@gmail.com>
2024-06-26 00:15:39 +0300

    added clarity small ui changes

---
+++ assets/addons.js
@@ -0,0 +1,30 @@
+<!-- microsoft clarity -->
+(function (c, l, a, r, i, t, y) {
+    c[a] = c[a] || function () {
+        (c[a].q = c[a].q || []).push(arguments)
+    };
+    t = l.createElement(r);
+    t.async = 1;
+    t.src = "https://www.clarity.ms/tag/" + i;
+    y = l.getElementsByTagName(r)[0];
+    y.parentNode.insertBefore(t, y);
+})
+(window, document, "clarity", "script", "mxnjub5utr"
+);
+
+<!-- Google tag (gtag.js) -->
+let imported = document.createElement('script');
+imported.async = true;
+imported.src = 'https://www.googletagmanager.com/gtag/js?id=G-20SXF8539L';
+document.head.appendChild(imported);
+
+window.dataLayer = window.dataLayer || [];
+
+function gtag() {
+    dataLayer.push(arguments);
+}
+
+gtag('js', new Date());
+
+gtag('config', 'G-20SXF8539L');
+
